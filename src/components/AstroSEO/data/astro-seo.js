dcfb9420aa453af7d735c0e37c39190a069a2d94
Jhossymar Contreras <chrisj_contreras@outlook.com>
2024-08-24 15:25:04 -0600

    build: :mag: Change to the way consume the seo-data.js to let the build works correctly

--- src/components/AstroSEO/data/astro-seo.js
+++ src/components/AstroSEO/data/astro-seo.js
@@ -1,141 +1,169 @@
-/*
-  If you need to add more properties you can read the documentation of Astro SEO at https://github.com/jonasmerlin/astro-seo#readme
-*/
-const seoData = {
-  charset: 'UTF-8',
-  description: '',
-  nofollow: false,
-  noindex: false,
-  titleDefault: 'Jhossymar Contreras',
-  titleTemplate: 'Jhossymar Contreras | %s',
-  link: {
-    svg: [{
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: '/favicon.svg'
-    }],
-    ico: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
-  },
-  meta: [
-    {
-      name: 'viewport',
-      content: 'width=device-width,user-scalable=no, initial-scale=1.0, minimum-scale=1.0'
+const pagesData = {
+  default: {
+    en: {
+      title: 'Page',
+      description: '',
+      openGraph: {
+        basic: {
+          title: '',
+          type: '',
+          image: ''
+        }
+      },
+      twitter: {
+        title: '',
+        image: '',
+        imageAlt: '',
+        description: ''
+      }
+    },
+    es: {
+      title: 'Página',
+      description: '',
+      openGraph: {
+        basic: {
+          title: '',
+          type: '',
+          image: ''
+        }
+      },
+      twitter: {
+        title: '',
+        image: '',
+        imageAlt: '',
+        description: ''
+      }
     }
-  ],
-  pages: {
-    default: {
-      en: {
-        title: 'Page',
-        description: '',
-        openGraph: {
-          basic: {
-            title: '',
-            type: '',
-            image: ''
-          }
-        },
-        twitter: {
+  },
+  home: {
+    en: {
+      title: 'Portfolio',
+      description: 'Web to show information about Jhossymar Contreras, projects, experience, skills, social networks',
+      openGraph: {
+        basic: {
           title: '',
-          image: '',
-          imageAlt: '',
-          description: ''
+          type: '',
+          image: ''
         }
       },
-      es: {
-        title: 'Página',
-        description: '',
-        openGraph: {
-          basic: {
-            title: '',
-            type: '',
-            image: ''
-          }
-        },
-        twitter: {
-          title: '',
-          image: '',
-          imageAlt: '',
-          description: ''
-        }
+      twitter: {
+        title: '',
+        image: '',
+        imageAlt: '',
+        description: ''
       }
     },
-    home: {
-      en: {
-        title: 'Portfolio',
-        description: 'Web to show information about Jhossymar Contreras, projects, experience, skills, social networks',
-        openGraph: {
-          basic: {
-            title: '',
-            type: '',
-            image: ''
-          }
-        },
-        twitter: {
+    es: {
+      title: 'Portafolio',
+      description: 'Web para mostrar información sobre Jhossymar Contreras, proyectos, experiencia, habilidades, redes sociales',
+      openGraph: {
+        basic: {
           title: '',
-          image: '',
-          imageAlt: '',
-          description: ''
+          type: '',
+          image: ''
         }
       },
-      es: {
-        title: 'Portafolio',
-        description: 'Web para mostrar información sobre Jhossymar Contreras, proyectos, experiencia, habilidades, redes sociales',
-        openGraph: {
-          basic: {
-            title: '',
-            type: '',
-            image: ''
-          }
-        },
-        twitter: {
+      twitter: {
+        title: '',
+        image: '',
+        imageAlt: '',
+        description: ''
+      }
+    }
+  },
+  'not-found': {
+    en: {
+      title: 'Not Found',
+      description: 'Page not found',
+      openGraph: {
+        basic: {
           title: '',
-          image: '',
-          imageAlt: '',
-          description: ''
+          type: '',
+          image: ''
         }
+      },
+      twitter: {
+        title: '',
+        image: '',
+        imageAlt: '',
+        description: ''
       }
     },
-    'not-found': {
-      en: {
-        title: 'Not Found',
-        description: 'Page not found',
-        openGraph: {
-          basic: {
-            title: '',
-            type: '',
-            image: ''
-          }
-        },
-        twitter: {
+    es: {
+      title: 'No Encontrado',
+      description: 'Página no encontrada',
+      openGraph: {
+        basic: {
           title: '',
-          image: '',
-          imageAlt: '',
-          description: ''
+          type: '',
+          image: ''
         }
       },
-      es: {
-        title: 'No Encontrado',
-        description: 'Página no encontrada',
-        openGraph: {
-          basic: {
-            title: '',
-            type: '',
-            image: ''
-          }
-        },
-        twitter: {
-          title: '',
-          image: '',
-          imageAlt: '',
-          description: ''
-        }
+      twitter: {
+        title: '',
+        image: '',
+        imageAlt: '',
+        description: ''
       }
     }
   }
 }
 
-export default seoData
+const createSEOTemplate = () => {
+  return {
+    charset: 'UTF-8',
+    description: '',
+    nofollow: false,
+    noindex: false,
+    title: 'Jhossymar Contreras | %s',
+    titleDefault: 'Jhossymar Contreras',
+    titleTemplate: 'Jhossymar Contreras | %s',
+    link: [],
+    meta: [],
+    openGraph: {
+      basic: {
+        title: '',
+        type: '',
+        image: ''
+      }
+    },
+    twitter: {
+      title: '',
+      image: '',
+      imageAlt: '',
+      description: ''
+    }
+  }
+}
+
+const addLink = (seoObject, rel, type, href) => {
+  seoObject.link.push({ rel, type, href })
+  return seoObject
+}
+
+const addMeta = (seoObject, name, content) => {
+  seoObject.meta.push({ name, content })
+  return seoObject
+}
+
+const setPageData = (seoObject, pageID, lang) => {
+  seoObject.title = pagesData[pageID][lang].title || seoObject.titleDefault
+  seoObject.description = pagesData[pageID][lang].description || seoObject.description
+  seoObject.openGraph = pagesData[pageID][lang].openGraph || seoObject.openGraph
+  seoObject.twitter = pagesData[pageID][lang].twitter || seoObject.twitter
+
+  return seoObject
+}
+
+const createSEOData = (pageID = 'default', lang = 'en') => {
+  let seoObject = createSEOTemplate()
+
+  seoObject = addLink(seoObject, 'icon', 'image/x-icon', '/favicon.ico')
+  // seoObject = addLink(seoObject, 'icon', 'image/svg+xml', '/favicon.svg')
+  seoObject = addMeta(seoObject, 'viewport', 'width=device-width,user-scalable=no, initial-scale=1.0, minimum-scale=1.0')
+  seoObject = setPageData(seoObject, pageID, lang)
+
+  return seoObject
+}
+
+export default createSEOData
